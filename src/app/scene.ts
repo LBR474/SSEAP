c8191c5003a497d3f7b46bdcfac0670167ed52a5
Fake Name <fakename@fake.com>
2023-05-02 17:00:41 +1000

    Evening commit

--- src/app/scene.ts
+++ src/app/scene.ts
@@ -1,16 +1,28 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ElementRef,
+  inject,
+  ViewChild,
+} from '@angular/core';
 import {
   extend,
   injectNgtLoader,
   NgtArgs,
   NgtBeforeRenderEvent,
   NgtPush,
+  NgtStore,
 } from 'angular-three';
 import { map } from 'rxjs';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Mesh } from 'three';
+import { OBJLoader, OrbitControls } from 'three-stdlib';
 
+import { gsap } from 'gsap';
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 extend(THREE);
+extend({ OrbitControls });
 
 @Component({
   standalone: true,
@@ -21,78 +33,136 @@ extend(THREE);
       penumbra="1"
       color="white"
     ></ngt-spot-light>
-    <ngt-point-light [position]="[0, 0, -10]"></ngt-point-light>
-    <ngt-point-light [position]="[0, 0, 10]"></ngt-point-light>
+    <ngt-point-light [position]="[0, 10, -10]"></ngt-point-light>
+    <ngt-point-light [position]="[0, 10, 10]"></ngt-point-light>
     <ngt-primitive
       *args="[model$ | ngtPush]"
       [scale]="1"
       (beforeRender)="onBeforeRender($any($event))"
+      #primitive
+    />
+    <ngt-orbit-controls
+      *args="[camera, glDom]"
+      [enableDamping]="true"
+      (beforeRender)="$any($event).object.update()"
     />
   `,
   imports: [NgtArgs, NgtPush],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Scene {
-  material1!: THREE.MeshStandardMaterial;
-  material2!: THREE.MeshStandardMaterial;
-    render_count = 0
+  private readonly store = inject(NgtStore);
+  readonly camera = this.store.get('camera');
+
+  readonly glDom = this.store.get('gl', 'domElement');
+
+  readonly events = this.store.get('events');
+
+  render_count = 0;
+
+  positionAdded = false;
+
   onBeforeRender(event: NgtBeforeRenderEvent<THREE.Mesh>) {
     const mesh = event.object as THREE.Mesh;
-    this.render_count++
-    if (this.render_count === 5 ){
-      
-       console.log(mesh.children[0]);
-    }
-    
-
     mesh.rotation.y += 0.01;
-    if (mesh.children[0].scale.y < 1.5) {
-      mesh.children[0].scale.y += 0.01;
-      // Change the color of each face of the mesh
-     
-      const t = mesh.children[0].scale.y / 1.5;
 
-      // Update material colors and make into white 
-      const hsl = new THREE.Color().setHSL(t, 1, 0.5);
-      const targetHSL = new THREE.Color().setHSL(t, 1, 1);
-      const deltaHSL = targetHSL.clone().sub(hsl);
-      const startTime = Date.now();
+    //(event.object.children[0] as THREE.Mesh).material;
 
-      const animateColor = () => {
-        const deltaTime = Date.now() - startTime;
-        const progress = Math.min(deltaTime / 3500, 1);
-        const currentHSL = hsl
-          .clone()
-          .add(deltaHSL.clone().multiplyScalar(progress));
-        this.material1.color.set(currentHSL);
-        this.material2.color.set(currentHSL);
+    let tl = gsap.timeline();
+    tl.to((event.object.children[1] as THREE.Mesh).material, {
+      onStart: () => {
+        gsap.to((event.object.children[0] as THREE.Mesh).material, {
+          opacity: '-=0.1',
+        });
+      },
 
-        if (progress < 1) {
-          requestAnimationFrame(animateColor);
-        }
-      };
+      opacity: 1,
+      delay: 3,
+    })
+      .to((event.object.children[2] as THREE.Mesh).material, {
+        onStart: () => {
+          gsap.to((event.object.children[1] as THREE.Mesh).material, {
+            opacity: 0,
+            duration: 1,
+          });
+        },
 
-      animateColor();
-    }
+        opacity: 1,
+        delay: 4,
+      })
+      .to((event.object.children[4] as THREE.Mesh).material, {
+        onStart: () => {
+          gsap.to((event.object.children[2] as THREE.Mesh).material, {
+            opacity: 0,
+            duration: 1,
+          });
+          console.log(event.object)
+        },
+
+        opacity: 1,
+        delay: 6,
+      });
+
+    // onComplete: () => {
+    //     event.object.children[0].visible = true;
+
+    //     //console.log(event.object);
+    //   },
+    // .to((event.object.children[0] as THREE.Mesh).material, {
+    //   onStart: () => {
+    //     gsap.to((event.object.children[1] as THREE.Mesh).material, {
+    //       opacity: '+=0.2',
+    //       duration: 2,
+    //       delay: 4,
+    //     });
+    //   },
+
+    //   opacity: '-=0.1',
+    //   duration: 1,
+    //   delay: 4,
+    // })
+    // .to((event.object.children[3] as THREE.Mesh).material, {
+    //   opacity: 1,
+    //   diration: 3,
+    //   delay: 6,
+    // })
+    // .to((event.object.children[4] as THREE.Mesh).material, {
+    //   opacity: 1,
+    //   diration: 3,
+    //   delay: 8,
+    // });
   }
 
   readonly model$ = injectNgtLoader(
     () => GLTFLoader,
-    'assets/smooth_sphere.glb'
+    'assets/smooth_sphere_7.glb'
   ).pipe(
     map((model) => {
-      this.material1 = (model.scene.children[0] as THREE.Mesh)
-        .material as THREE.MeshStandardMaterial;
-      this.material2 = (model.scene.children[1] as THREE.Mesh)
-        .material as THREE.MeshStandardMaterial;
+      // Apply the new material to each sphere
+      model.scene.traverse((child) => {
+        if (child instanceof THREE.Mesh && child.name.includes('001')) {
+          // child.scale.x = 0.5;
+          // child.scale.y = 0.5;
+          // child.scale.z = 0.5;
 
-       const geometry = new THREE.PlaneGeometry(1, 0.05);
-       const material = new THREE.MeshBasicMaterial({ color: 0x000000 });
-       const line = new THREE.Mesh(geometry, material);
-       line.position.set(0, 1.5, 0);
-       model.scene.add(line);
+          child.material.transparent = true;
+          child.material.opacity = 1;
+        }
+        if (child instanceof THREE.Mesh && child.name.includes('002')) {
+          child.material.transparent = true;
+          child.material.opacity = 0;
+        }
+        if (child instanceof THREE.Mesh && child.name.includes('003')) {
+          child.material.transparent = true;
+          child.material.opacity = 0;
+        }
+        if (child instanceof THREE.Mesh && child.name.includes('004')) {
+          child.material.transparent = true;
+          child.material.opacity = 0;
+        }
+      });
 
-       return model.scene;
+      return model.scene;
     })
   );
 }
